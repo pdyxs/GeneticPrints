e8092e5eaf8448462dcf8882f71afce14eb4c304
Paul Sztajer <paul@sztajer.org>
2018-03-26 19:03:33 +0700

    attempt 3

--- readsequence.py
+++ readsequence.py
@@ -2,6 +2,11 @@ import os
 import math
 from PIL import Image, ImageDraw
 
+GENEFILE = "ncbiGene.txt"
+FILENAME = "KM034562v1.fa"
+VERSION = 3
+OUTPUT_FOLDER = "out/"
+
 class Nucleotide:
     def __repr__(self):
         return self.character
@@ -16,10 +21,10 @@ class Nucleotide:
         self.colour = colour
 
 NUCLEOTIDES = {
-    'A': Nucleotide('A', 'T', "rgb(100,0,0)"),
-    'T': Nucleotide('T', 'A', "rgb(200,0,0)"),
-    'G': Nucleotide('G', 'C', "rgb(0,0,100)"),
-    'C': Nucleotide('C', 'G', "rgb(0,0,200)")
+    'A': Nucleotide('A', 'T', "rgb(200,0,0)"),
+    'T': Nucleotide('T', 'A', "rgb(200,100,0)"),
+    'G': Nucleotide('G', 'C', "rgb(0,0,200)"),
+    'C': Nucleotide('C', 'G', "rgb(0,100,200)")
 }
 
 class Sequence:
@@ -27,7 +32,11 @@ class Sequence:
 
     def __init__(self, nucleotideString, genes):
         self.loadNucleotides(nucleotideString)
-        self.genes = genes
+        self.genes = []
+        for i, gene in enumerate(genes):
+            if (i > 0) and (gene.transcriptionRegion[0] == genes[i-1].transcriptionRegion[0]):
+                continue
+            self.genes.append(gene)
 
     def loadNucleotides(self, nucleotideString):
         self.nucleotides = []
@@ -36,11 +45,6 @@ class Sequence:
 
     def length(self):
         return len(self.nucleotides)
-
-    def draw(self, draw, width):
-        for counter, nucleotide in enumerate(self.nucleotides):
-            coords = (counter % width, math.floor(counter / width))
-            draw.point([coords], nucleotide.colour)
 
 class Gene:
     'A Gene'
@@ -61,6 +65,14 @@ class Gene:
         for i in range(int(exonCount)):
             self.exons.append((int(exonStarts[i]), int(exonEnds[i])))
 
+    def isExon(self, index):
+        for (s, e) in self.exons:
+            if index < s:
+                return False
+            if index < e:
+                return True
+        return False
+
     def gaps(self):
         gaps = 0
         for i in range(len(self.exons)):
@@ -79,11 +91,6 @@ def readFileLines(filename, startObject, lineFunc):
         startObject = lineFunc(line, startObject)
     return startObject
 
-GENEFILE = "ncbiGene.txt"
-FILENAME = "KM034562v1.fa"
-VERSION = 3
-OUTPUT_FOLDER = "out/"
-
 pairString = readFileLines(FILENAME, "",
     lambda l, o: o if l.startswith(">") else o + l.strip());
 
@@ -96,15 +103,90 @@ genes = readFileLines(GENEFILE, [],
 
 sequence = Sequence(pairString, genes)
 
-IMAGE_WIDTH = math.ceil(math.sqrt(sequence.length()))
-IMAGE_SIZE = (IMAGE_WIDTH, IMAGE_WIDTH)
+def getWidth(sequence):
+    extents = [0,0]
+    pos = 0
+    for count, gene in enumerate(sequence.genes):
+        width = gene.width()
+        overlap = 0
+        if count > 0:
+            overlap = max(0, sequence.genes[count - 1].transcriptionRegion[1] - gene.transcriptionRegion[0])
+            overlap = (overlap + 1) % 2
+        if count % 2 == 0:
+            pos += overlap
+            pos = pos + width
+            extents[1] = max(extents[1], pos)
+        else:
+            pos = pos - width
+            extents[0] = min(extents[0], pos)
+    return (- extents[0], (extents[1] - extents[0]) + 2)
+
+def getHeight(seq):
+    pos = 0
+    height = 0
+    for i, gene in enumerate(seq.genes):
+        if pos < gene.transcriptionRegion[0]:
+            height = height + gene.transcriptionRegion[0] - pos
+        else:
+            height = height + pos - gene.transcriptionRegion[0]
+        pos = gene.transcriptionRegion[1]
+    height += seq.length() - seq.genes[-1].transcriptionRegion[1]
+    return height + 3
+
+(startPos, IMAGE_WIDTH) = getWidth(sequence)
+IMAGE_SIZE = (IMAGE_WIDTH, getHeight(sequence))
+print(IMAGE_SIZE)
+print(sequence.nucleotides[-1])
 
 im = Image.new('RGBA', IMAGE_SIZE)
 draw = ImageDraw.Draw(im)
 
+def drawSequence(draw,seq,startPos):
+    pos = (startPos, 0)
+    spos = 0
+    for i, gene in enumerate(seq.genes):
+        while spos < gene.transcriptionRegion[0]:
+            draw.point([pos], seq.nucleotides[spos].colour)
+            spos += 1
+            pos = (pos[0], pos[1] + 1)
+        while spos < gene.codingRegion[0]:
+            draw.point([pos], seq.nucleotides[spos].colour)
+            spos += 1
+            pos = (pos[0] + (1 if i % 2 == 0 else -1), pos[1])
+        while spos < gene.codingRegion[1]:
+            if gene.isExon(spos):
+                draw.point([(pos[0],pos[1]-1)], seq.nucleotides[spos].colour)
+                draw.point([(pos[0],pos[1])], seq.nucleotides[spos+1].colour)
+                draw.point([(pos[0],pos[1]+1)], seq.nucleotides[spos+2].colour)
+                spos += 3
+                pos = (pos[0] + (1 if i % 2 == 0 else -1), pos[1])
+            else:
+                draw.point([pos], seq.nucleotides[spos].colour)
+                spos += 1
+                pos = (pos[0] + (1 if i % 2 == 0 else -1), pos[1])
+        while spos < gene.transcriptionRegion[1]:
+            draw.point([pos], seq.nucleotides[spos].colour)
+            spos += 1
+            if i < len(seq.genes) - 1 and spos >= seq.genes[i+1].transcriptionRegion[0]:
+                mid = (seq.genes[i+1].transcriptionRegion[0] + gene.transcriptionRegion[1]) / 2
+                if spos < mid:
+                    pos = (pos[0] + (1 if i % 2 == 0 else -1), pos[1] + 1)
+                else:
+                    pos = (pos[0] + (1 if i % 2 == 1 else -1), pos[1] + 1)
+            else:
+                pos = (pos[0] + (1 if i % 2 == 0 else -1), pos[1])
+    while spos < seq.length():
+        draw.point([pos], seq.nucleotides[spos].colour)
+        spos += 1
+        pos = (pos[0], pos[1] + 1)
+    print(pos)
+
+drawSequence(draw, sequence, startPos)
+
+print(sequence.genes)
+print(sequence.length())
+
 if not os.path.exists(OUTPUT_FOLDER):
     os.makedirs(OUTPUT_FOLDER)
-
-sequence.draw(draw, IMAGE_WIDTH)
 
 im.save(OUTPUT_FOLDER + FILENAME + '.' + str(VERSION) + '.png')
